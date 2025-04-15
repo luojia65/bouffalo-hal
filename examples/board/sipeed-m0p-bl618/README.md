bbf87a23cc578c2fff9001839c9452976cac4211
Zhouqi Jiang <luojia@hust.edu.cn>
2025-04-15 11:10:03 +0800

    feat(examples): add Sipeed M0P BL618 examples
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

---
+++ examples/board/sipeed-m0p-bl618/README.md
@@ -0,0 +1,9 @@
+# Sipeed M0P BL618 board
+
+## Run example
+
+blink:
+
+```
+cargo run -p sipeed-m0p-bl618 --bin blink --target riscv64imac-unknown-none-elf
+```
---
+++ examples/board/sipeed-m0p-bl618/src/bin/blink.rs
@@ -0,0 +1,17 @@
+#![no_std]
+#![no_main]
+
+use bouffalo_hal::prelude::*;
+use bouffalo_rt::{Clocks, Peripherals, entry};
+use panic_halt as _;
+
+#[entry]
+fn main(p: Peripherals, _c: Clocks) -> ! {
+    let mut led = p.gpio.io21.into_floating_output();
+    let mut led_state = PinState::High;
+    loop {
+        led.set_state(led_state).ok();
+        led_state = !led_state;
+        riscv::asm::delay(100_000);
+    }
+}
