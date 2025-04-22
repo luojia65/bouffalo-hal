d80770b30516091d12c9715342dd4ece8d165709
Zhouqi Jiang <luojia@hust.edu.cn>
2025-04-22 11:43:16 +0800

    fix(rt): use #[unsafe(naked)] macro which is to be stablized in rustc
    Naked functions are to be stablized in this pull request: https://github.com/rust-lang/rust/pull/134213
    
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- bouffalo-rt/src/soc/bl616.rs
+++ bouffalo-rt/src/soc/bl616.rs
@@ -4,7 +4,7 @@ use crate::{HalBasicConfig, HalFlashConfig, HalPatchCfg};
 use core::ops::Deref;
 
 #[cfg(all(feature = "bl616", target_arch = "riscv32"))]
-#[naked_function::naked]
+#[unsafe(naked)]
 #[link_section = ".text.entry"]
 #[export_name = "_start"]
 unsafe extern "C" fn start() -> ! {
